89b02466468ebb781839c48fa51bb33d864fe8b1
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2020-11-25 16:43:22 -0600

    Clean-up of unnecessary code

--- js/symb/collections.harvestparams.js
+++ js/symb/collections.harvestparams.js
@@ -16,7 +16,7 @@ function checkHarvestParamsForm(frm){
 	if((frm.taxa.value.trim() == '') && (frm.country.value.trim() == '') && (frm.state.value.trim() == '') && (frm.county.value.trim() == '') &&
 		(frm.local.value.trim() == '') && (frm.elevlow.value.trim() == '') && (frm.upperlat.value.trim() == '') && (frm.footprintwkt.value.trim() == '') && (frm.pointlat.value.trim() == '') &&
 		(frm.collector.value.trim() == '') && (frm.collnum.value.trim() == '') && (frm.eventdate1.value.trim() == '') && (frm.catnum.value.trim() == '') &&
-		(frm.typestatus.checked == false) && (frm.hasimages.checked == false) && (frm.hasgenetic.checked == false) && (frm.isreproductive.checked == false) && (frm.issterile.checked == false) && (frm.isnotscorable.checked == false)){
+		(frm.typestatus.checked == false) && (frm.hasimages.checked == false) && (frm.hasgenetic.checked == false)){
 		alert("Please fill in at least one search parameter!");
 		return false;
 	}
