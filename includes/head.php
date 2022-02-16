1eea28ace36db2478ebf1c06bbb4f35cb69931a3
Laura Rocha Prado <lauraprado@asu.edu>
2022-02-16 13:37:41 -0700

    adds normalize (slim version)

--- includes/head.php
+++ includes/head.php
@@ -1,16 +1,20 @@
 <?php
 /*
- * Customize styling by adding or modifying CSS file links below
- * Default styling for individual page is defined within /css/symb/
- * Individual styling can be customized by:
- *     1) Uncommenting the $CUSTOM_CSS_PATH variable below
- *     2) Copying individual CCS file to the /css/symb/custom directory
- *     3) Modifying the sytle definiation within the file
+ * * CHH2 Custom Head
+ * * Version: 2.0 February 2022
+ * * Author: Laura Rocha Prado
+ * * Author URI: http://laura.rochaprado.com
+ * * License: GNU General Public License v2 or later
+ * * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 //$CUSTOM_CSS_PATH = '/css/symb/custom';
 ?>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
 <link href="<?php echo $CLIENT_ROOT; ?>/css/jquery-ui.css" type="text/css" rel="stylesheet">
+<!-- Symbiota defaults -->
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
-<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=1" type="text/css" rel="stylesheet">
+<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=2" type="text/css" rel="stylesheet">
+<!-- Normalize -->
+<link href="<?php echo $CLIENT_ROOT; ?>/css/custom/normalize.slim.css?ver=1" type="text/css" rel="stylesheet">
+
