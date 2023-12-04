c8354feda0d5a957f87a03a482b824f8ec8d1c0e
Edward Gilbert <egbot@asu.edu>
2023-12-04 09:57:33 -0700

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -13,6 +13,6 @@
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
 
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
-<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=2" type="text/css" rel="stylesheet">
+<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=3" type="text/css" rel="stylesheet">
 
 <script src="<?php echo $CLIENT_ROOT; ?>/js/symb/lang.js" type="text/javascript"></script>
